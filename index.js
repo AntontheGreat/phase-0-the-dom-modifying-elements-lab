6a95d9cef14ef25f30a42adbce25051fc6ef2921
Antonio <antonio18casta@aol.com>
2022-07-13 17:03:55 -0400

    commit tree d05412b2e89f0bc7a7c3c6bea0fe37c9c17705be

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//#1
+main.remove();
+
+//#2
+const newHeader = document.createElement("h1");
+
+//#3
+newHeader.id = "victory";
+
+//#4
+newHeader.textContent = "Antonio is the champion";
